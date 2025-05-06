a6ec4da21991ce04c05dd27e40095f4be124ff77
Prajit Viswanadha <prajit.viswanadha.06@gmail.com>
2025-05-06 17:19:12 -0500

    Stop tracking Frontend/dist and add it to .gitignore

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+
   build: {
     rollupOptions: {
-      external: ['stockfish'],
+      //external: ['stockfish'],
     },
   },
-  optimizeDeps: {
-    exclude: ['stockfish'],
-  },
+  // optimizeDeps: {
+  //   exclude: ['stockfish'],
+  // },
 })
